278b54f0c3ba05384141674754554d6a190d7034
Jorge Perez de Acha <perezde@cshl.edu>
2017-09-26 16:33:47 -0400

    Updates to db schema and adapted ticker.sh to run in Linux

--- db_schema.sh
+++ db_schema.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ -z "$AGAVE_TICKER_DB" ]; then
-    echo "You need to set \$AGAVE_TICKER_DB var.."
-    exit 0
+    echo $(basename $0) "You need to set \$AGAVE_TICKER_DB var.."
+    exit 1
 fi
 
 #create db
@@ -14,5 +14,8 @@ create table current(id char(37) PRIMARY KEY);
 
 -- 'finished' stores the ids of the jobs that have finished (i.e. job's status is FINISHED, KILLED, FAILED, or STOPPED)
 create table completed(id char(37) PRIMARY KEY, status varchar(8) NOT NULL, name varchar(50) NOT NULL, owner varchar(50) NOT NULL, appId varchar(50) NOT NULL, created datetime NOT NULL, submitTime datetime NOT NULL, startTime datetime, endtime datetime);
+
+-- 'files_log' stores operations performed on files and directories 
+CREATE TABLE files_log(operation VARCHAR(50) NOT NULL, path TEXT, date DATETIME NOT NULL, status INT);
 
 EOS
--- ticker.sh
+++ ticker.sh
@@ -14,20 +14,20 @@ fi
 # check if db exists. Else creates database
 if [ ! -s $AGAVE_TICKER_DB ] 
 then
-	./db_schema.sh
+	. ./db_schema.sh
 fi
 
 # submit job and keep job id
-id=`jobs-submit -F $AGAVE_TICKER_JOB_FILE  | sed 's/Successfully submitted job //'`
+id=$(jobs-submit -F $AGAVE_TICKER_JOB_FILE  | sed 's/Successfully submitted job //')
 
 
 if [ ! -z "$id" ]
 then
 	# store id into db
 	(sqlite3 $AGAVE_TICKER_DB "INSERT INTO current VALUES('$id')") \
-		|| echo `date +%Y-%m-%d%n%H:%M:%S` "Failure to insert id $id into CURRENT"
+		|| echo $(date +%Y-%m-%d%n%H:%M:%S) "Failure to insert id $id into CURRENT"
 else
-	echo `date +%Y-%m-%d%n%H:%M:%S` "No id produced"
+	echo $(date +%Y-%m-%d%n%H:%M:%S) "No id produced"
 fi
 
 # check job status for each job in 'current'
@@ -35,25 +35,25 @@ ids=$(sqlite3 $AGAVE_TICKER_DB "SELECT * FROM current")
 
 for id in $ids 
 do
-	status=`jobs-status $id`
+	status=$(jobs-status $id)
 
 	if [ "$status" == "FINISHED" ] || [ "$status" == "KILLED" ] || [ "$status" == "FAILED" ] || [ "$status" == "STOPPED" ]
 	then
-		jobsList=`jobs-list -v $id`
-		name=`echo $jobsList | jq '.name'`
-		owner=`echo $jobsList | jq '.owner'`
-		appId=`echo $jobsList | jq '.appId'`
-		created=`echo $jobsList | jq '.created'`
-		submitTime=`echo $jobsList | jq '.submitTime'`
-		startTime=`echo $jobsList | jq '.startTime'`
-		endTime=`echo $jobsList | jq '.endTime'`
+		jobsList=$(jobs-list -v $id)
+		name=$(echo $jobsList | jq '.name')
+		owner=$(echo $jobsList | jq '.owner')
+		appId=$(echo $jobsList | jq '.appId')
+		created=$(echo $jobsList | jq '.created')
+		submitTime=$(echo $jobsList | jq '.submitTime')
+		startTime=$(echo $jobsList | jq '.startTime')
+		endTime=$(echo $jobsList | jq '.endTime')
 		
 		( 
 		 # inserts id and job information into COMPLETED 
 		 # if the data was successfully inserted into COMPLETED, the id is deleted from CURRENT. Else, an error message is displayed
 		(sqlite3 $AGAVE_TICKER_DB "INSERT INTO completed VALUES('$id', '$status', $name, $owner, $appId, $created, $submitTime, $startTime, $endTime)") && \
 				( (sqlite3 $AGAVE_TICKER_DB "DELETE FROM current WHERE id='$id'") || \
-					echo `date +%Y-%m-%d%n%H:%M:%S` "Failure to delete $id from CURRENT") ) || \
+					echo $(date +%Y-%m-%d%n%H:%M:%S) "Failure to delete $id from CURRENT") ) || \
 			( 
 			# there are two situations in which id and job information cannot be inserted into COMPLETED:  
 			# 1. Failure with db connection 
@@ -62,7 +62,7 @@ do
 			if grep $id <<< $ids2
 			# if id is in both CURRENT and COMPLETED, then id is deleted from CURRENT. Else, an error message is desplayed.
 				then (sqlite3 $AGAVE_TICKER_DB "DELETE FROM current WHERE id='$id'") 
-				else echo `date +%Y-%m-%d%n%H:%M:%S` "Failure to insert $id into COMPLETED"
+				else echo $(date +%Y-%m-%d%n%H:%M:%S) "Failure to insert $id into COMPLETED"
 			fi
 		)
 
